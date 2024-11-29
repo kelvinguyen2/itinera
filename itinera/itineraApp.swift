16920d1921979ba1319cf075efc4009d7fe4fc1c
kelvin <kelvinguyen2@gmail.com>
2024-11-28 20:13:33 -0800

    Initial Commit

---
+++ itinera/itineraApp.swift
@@ -0,0 +1,17 @@
+//
+//  itineraApp.swift
+//  itinera
+//
+//  Created by Kelvin Nguyen on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct itineraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ itineraTests/itineraTests.swift
@@ -0,0 +1,17 @@
+//
+//  itineraTests.swift
+//  itineraTests
+//
+//  Created by Kelvin Nguyen on 11/28/24.
+//
+
+import Testing
+@testable import itinera
+
+struct itineraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
